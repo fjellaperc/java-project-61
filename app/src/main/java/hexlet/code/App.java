1b875b11dabdab621a54fe8a1ef0dad8ec511235
fjellaperc <fjellaperc@gmail.com>
2023-05-30 18:46:32 +0300

    fix App.java and Cli.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,45 @@
 package hexlet.code;
+import hexlet.code.games.Calculate;
+import hexlet.code.games.Even;
+import hexlet.code.games.Prime;
+import hexlet.code.games.Progression;
+import hexlet.code.games.Welcome;
+import hexlet.code.games.GCD;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-        Cli.outUser();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int userInt = scanner.nextInt();
+            System.out.println("Your choice: " + userInt);
+            if (userInt == 1) {
+                Welcome.welcome();
+            } else if (userInt == 2) {
+                Even.evenNum();
+            } else if (userInt == 3) {
+                Calculate.calc();
+            } else if (userInt == 4) {
+                GCD.gcd();
+            } else if (userInt == 5) {
+                Progression.progression();
+            } else if (userInt == 6) {
+                Prime.prime();
+            } else {
+                System.out.println("Please, enter correct option!!!");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Please, enter correct option!!!");
+        }
     }
 }
--- app/src/main/java/hexlet/code/games/Cli.java
+++ app/src/main/java/hexlet/code/games/Cli.java
@@ -1,13 +1,12 @@
-package hexlet.code;
+package hexlet.code.games;
 import java.util.Scanner;
 
 public class Cli {
-    public static void Cli() {
+    public static void cli() {
         System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? " );
+        System.out.print("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
         String nameUser = scanner.next();
-        //scanner.close();
         System.out.println("Hello, " + nameUser);
     }
 }
