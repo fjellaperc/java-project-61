f47265993f2f9a973196ca822fd142fc3706a8bb
fjellaperc <fjellaperc@gmail.com>
2023-07-12 14:31:33 +0300

    fix Progression

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -9,35 +9,28 @@ public class Progression {
     public static final int COUNT_STEP = 3; //Количество раундов в игре
     public static final int TOP_RANGE_SIZE = 10; //Маскимальная длина массива
     public static final int LOW_RANGE_SIZE = 6; //Минимальная длина массива
-    public static String[] textureProgression() {
-        int lengthArray = Random.generateNum(LOW_RANGE_SIZE, TOP_RANGE_SIZE); //Длина массива
-        int emptyElement = Random.generateNum(1, lengthArray); //Позиция скрытого элемента(от 1 до 7)
-        String[] numbers = new String[lengthArray];
-        String[] compareNumbers = new String[lengthArray];
-        String[] outArray = new String[2];
-        int currentElement = Random.generateNum(1, RANGE_FIRST_NUM); //Задаем значение 1ого элемента
-        int step = Random.generateNum(1, TOP_RANGE_STEP); //Шаг прогрессии (от 1 до 4)
-        numbers[0] = Integer.toString(currentElement);
-        compareNumbers[0] = Integer.toString(currentElement);
-        for (int i = 1; i < numbers.length; i++) {  //Формируем массив прогрессии
-            currentElement = currentElement + step;
-            if (i == emptyElement) {
-                numbers[i] = "..";
-            } else {
-                numbers[i] = Integer.toString(currentElement);
-            }
-            compareNumbers[i] = Integer.toString(currentElement);
+    public static String[] createProgression() {
+        int lengthArray = Random.generateNum(LOW_RANGE_SIZE, TOP_RANGE_SIZE);
+        int currentElement = Random.generateNum(1, RANGE_FIRST_NUM);
+        int step = Random.generateNum(1, TOP_RANGE_STEP);
+        String[] outNumbers = new String[lengthArray];
+        outNumbers[0] = Integer.toString(currentElement);
+        for (int i = 1; i < lengthArray; i++) {
+             currentElement = currentElement + step;
+             outNumbers[i] = Integer.toString(currentElement);
         }
-        String question = String.join(" ", numbers);
-        outArray[0] = question;
-        outArray[1] = compareNumbers[emptyElement];
-        return outArray;
+        return outNumbers;
     }
     public static void runProgressionGame() {
         String rules = "What number is missing in the progression?";
         String[][] resultArray = new String[COUNT_STEP][2];
-        for (int j = 0; j < COUNT_STEP; j++) {
-            resultArray[j] = textureProgression();
+        for (int i = 0; i < COUNT_STEP; i++) {
+            String[] numbers = createProgression();
+            int emptyElement = Random.generateNum(1, numbers.length); //Позиция пустого элемента
+            resultArray[i][1] = numbers[emptyElement]; //Записали правильный ответ
+            numbers[emptyElement] = "..";
+            String question = String.join(" ", numbers);
+            resultArray[i][0] = question;
         }
         Engine.run(rules, resultArray);
     }
